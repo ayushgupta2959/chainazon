0953b968df48de23aa00033cd37b269b0698ae88
ayushgupta2959 <ayushgupta2959@gmail.com>
2024-06-01 16:13:33 +0530

    added list and buy for products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
+import Navigation from "./components/Navigation";
+import Section from "./components/Section";
+import Product from "./components/Product";
 
 // ABIs
-import Chainazon from './abis/Chainazon.json'
+import Chainazon from "./abis/Chainazon.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [chainazon, setChainazon] = useState(null);
+
+  const [account, setAccount] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+
+  const [item, setItem] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const togglePop = (item) => {
+    setItem(item);
+    toggle ? setToggle(false) : setToggle(true);
+  };
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const chainazon = new ethers.Contract(
+      config[network.chainId].chainazon.address,
+      Chainazon,
+      provider
+    );
+    setChainazon(chainazon);
+
+    const items = [];
+
+    for (var i = 0; i < 9; i++) {
+      const item = await chainazon.items(i + 1);
+      items.push(item);
+    }
+
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <h2>Chainazon Best Sellers</h2>
+      {electronics && clothing && toys && (
+        <>
+          <Section
+            title={"Clothing & Jewelry"}
+            items={clothing}
+            togglePop={togglePop}
+          />
+          <Section
+            title={"Electronics & Gadgets"}
+            items={electronics}
+            togglePop={togglePop}
+          />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
 
-      <h2>Welcome to Chainazon</h2>
-
+      {toggle && (
+        <Product
+          item={item}
+          provider={provider}
+          account={account}
+          chainazon={chainazon}
+          togglePop={togglePop}
+        />
+      )}
     </div>
   );
 }
